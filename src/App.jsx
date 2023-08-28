eb9eb6f67256a1f5711f1f787de7d42dd6cbda9e
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-08-28 07:05:44 +0600

    set up done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+       <h1 className="text-5xl font-bold underline p-font">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
